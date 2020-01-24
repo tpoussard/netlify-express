4041bca4aa804dc328364a5ad76da1e7bb45ff45
timothee.poussard <timothee.poussard@yahoo.com>
2020-01-24 20:58:57 +0100

    try to send a basic json 4

--- express/server.js
+++ express/server.js
@@ -1,47 +1,33 @@
 'use strict';
 const express = require('express');
+const path = require('path');
+const serverless = require('serverless-http');
 const app = express();
-// const port = process.env.PORT || 5000;
+const bodyParser = require('body-parser');
+const jsonData = require('./fr.json')
 
-// console.log that your server is up and running
-// app.listen(port, () => console.log(`Listening on port ${port}`));
+const router = express.Router();
 
-// create a GET route
-const data = [
-  {
-  "id": 1,
-  "category_name": "Documents importants",
-  "name": "Carte d'identité",
-  "category": 1
-},
-{
-  "id": 2,
-  "category_name": "Documents importants",
-  "name": "Passeport",
-  "category": 1
-},
-{
-  "id": 3,
-  "category_name": "Documents importants",
-  "name": "Visa",
-  "category": 1
-},
-{
-  "id": 4,
-  "category_name": "Documents importants",
-  "name": "Carnet de vaccination",
-  "category": 1
-}]
+const data = jsonData.categories
 
-app.get('/data', (req, res) => {
+/* router.get('/', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<h1>Hello from Express.js!</h1>');
+  res.end();
+}); */
+
+router.get('/', (req, res) => {
   let now = new Date();
   console.log("Data has been sent !", now);
   res.send(data)
 })
 
-const server = app.listen(5000, function() {
-    const host = server.address().address
-    const port = server.address().port
+router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
+router.post('/', (req, res) => res.json({ postBody: req.body }));
 
-    console.log('Backend server listening at http://%s:%s', host, port)
-})
+app.use(bodyParser.json());
+app.use('/.netlify/functions/server', router);  // path must route to lambda
+app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+
+module.exports = app;
+module.exports.handler = serverless(app);
